cd39bea4669b62efd698a376012a73522bd2d105
ulrichf <ulrichf@localhost>
2018-04-09 23:33:31 +0200

    #6

--- layer-optimization/build-images.sh
+++ layer-optimization/build-images.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-docker build --build-arg http_proxy=http://16.46.16.11:8080 -t opensuse-plain-unsquashed -f docker-files/plain.Dockerfile .
-docker build --build-arg http_proxy=http://16.46.16.11:8080 --squash -t opensuse-plain-squashed -f docker-files/plain.Dockerfile .
+prox=""
 
-#docker build --build-arg http_proxy=http://16.46.16.11:8080 -t opensuse-jdk-suse-unsquashed -f docker-files/openjdk-suse.Dockerfile .
-#docker build --build-arg http_proxy=http://16.46.16.11:8080 --squash -t opensuse-jdk-suse-squashed -f docker-files/openjdk-suse.Dockerfile .
+result=`ping -c 1 16.46.16.11 | grep "1 received"`
 
-#docker build --build-arg http_proxy=http://16.46.16.11:8080 -t opensuse-jdk-head-unsquashed -f docker-files/openjdk-head.Dockerfile .
-#docker build --build-arg http_proxy=http://16.46.16.11:8080 --squash -t opensuse-jdk-head-squashed -f docker-files/openjdk-head.Dockerfile .
+if [ $result!="" ];
+    then
+        prox="http://16.46.16.11:8080"
+fi
 
-#docker build --build-arg http_proxy=http://16.46.16.11:8080 -t opensuse-jdk-zulu-unsquashed -f docker-files/openjdk-zulu.Dockerfile .
-#docker build --build-arg http_proxy=http://16.46.16.11:8080 --squash -t opensuse-jdk-zulu-squashed -f docker-files/openjdk-zulu.Dockerfile .
+
+docker build --build-arg http_proxy=$prox -t opensuse-plain-unsquashed -f docker-files/plain.Dockerfile .
+docker build --build-arg http_proxy=$prox  --squash -t opensuse-plain-squashed -f docker-files/plain.Dockerfile .
+
+docker build --build-arg http_proxy=$prox  -t opensuse-jdk-suse-unsquashed -f docker-files/openjdk-suse.Dockerfile .
+docker build --build-arg http_proxy=$prox  --squash -t opensuse-jdk-suse-squashed -f docker-files/openjdk-suse.Dockerfile .
+
+#docker build --build-arg http_proxy=$prox  -t opensuse-jdk-head-unsquashed -f docker-files/openjdk-head.Dockerfile .
+#docker build --build-arg http_proxy=$prox  --squash -t opensuse-jdk-head-squashed -f docker-files/openjdk-head.Dockerfile .
+
+#docker build --build-arg http_proxy=$prox  -t opensuse-jdk-zulu-unsquashed -f docker-files/openjdk-zulu.Dockerfile .
+#docker build --build-arg http_proxy=$prox  --squash -t opensuse-jdk-zulu-squashed -f docker-files/openjdk-zulu.Dockerfile .
